29ca43256d38db220b417697de6ffdae6d9955d0
Mayank Rana <mayankranax1@gmail.com>
2024-10-15 08:59:42 +0530

    Add commit

--- apps/users/src/controllers/users.controller.ts
+++ apps/users/src/controllers/users.controller.ts
@@ -14,6 +14,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 
+//TODO: Need guards for routes that require authentication
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
